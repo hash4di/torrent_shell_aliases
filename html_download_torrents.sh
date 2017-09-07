f310f31ba3a9c0e5bb983c71d7c7ec65468411ef
Marcin Nowicki <pr0d1r2@gmail.com>
2017-09-07 05:03:16 +0200

    Conform to ShellCheck

--- html_download_torrents.sh
+++ html_download_torrents.sh
@@ -2,7 +2,7 @@ function html_download_torrents() {
   parallel \
     'cat {} | grep torrent | sed -e "s/href=/|/g" | tr "|" "\n" | grep torrent | cut -f 2 -d "\""' \
     ::: \
-    $@ 2>/dev/null | \
+    "$@" 2>/dev/null | \
     parallel \
       -v \
       "aria2c --bt-save-metadata --seed-time=60 -q {}" 2>/dev/null
