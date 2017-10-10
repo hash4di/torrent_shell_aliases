2c9e7a21e350e2e8a65579aa050036424e05c289
Marcin Nowicki <pr0d1r2@gmail.com>
2017-10-10 07:41:54 +0200

    Allow to download from Humble Bundle html

--- html_list_links_with_suffix.sh
+++ html_list_links_with_suffix.sh
@@ -4,7 +4,7 @@ function html_list_links_with_suffix() {
     return 200
   fi
   parallel \
-    "cat {} | grep '$SUFFIX' | sed -e 's/href=/|/g' | tr '|' '\\n' | cut -f 2 -d '\"' | grep '$SUFFIX$' | sort | uniq" \
+    "cat {} | grep '$SUFFIX' | sed -e 's/href=/|/g' | tr '|' '\\n' | cut -f 2 -d '\"' | grep '\\.$SUFFIX' | sed -e 's/\\&amp;/\\&/g' | sort | uniq" \
     ::: \
     "$@" 2>/dev/null
   return $?
